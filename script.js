c92f3bc2d31f50308bce6b68a590414a1effb342
Dronrus86 <pop4666@yandex.ru>
2021-07-19 19:15:04 +0300

    adding and removing js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 const addButton = document.querySelector('.add');
 const todoList = document.querySelector('.to-do_list');
 const options = document.querySelector('.options');
+const removeButton = document.querySelector('.pushbutton_delete');
+
+
+
 
 
 function removeElements(rem) {
@@ -16,13 +20,14 @@ addButton.addEventListener('click', () => {
     newElement.innerHTML = options.innerHTML;
     newElement.classList.add('options');
     todoList.append(newElement);
-    let deleteButton = newElement.querySelector('.pushbutton_delete');
-    deleteButton.addEventListener('click', () => {
+    let removeButton = newElement.querySelector('.pushbutton_delete');
+    removeButton.addEventListener('click', () => {
         removeElements(newElement);
     });
 
-
 });
 
-
+removeButton.addEventListener('click', () => {
+    removeElements(options);
+});
 
