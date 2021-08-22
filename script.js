c5b83283d667f1ea191c450664381878348f310f
Dronrus86 <pop4666@yandex.ru>
2021-08-22 21:09:07 +0300

    delete

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const addButton = document.querySelector('.add');
 const todoList = document.querySelector('.to-do_list');
 const options = document.querySelector('.options');
 const removeButton = document.querySelector('.pushbutton_delete');
-
-
-
+const arrow = document.querySelector('.sort_arrow');
+const triangle = document.querySelector('.sorting_triangle');
+let inputData = document.querySelector('.list_points');
+let sortButton = document.querySelector('.sorting');
+const plus = document.querySelector('.plus');
 
 
 function removeElements(rem) {
@@ -14,6 +16,8 @@ function removeElements(rem) {
         rem.remove();
    }
 }
+
+
 
 addButton.addEventListener('click', () => {
     let newElement = document.createElement('div');
@@ -27,7 +31,71 @@ addButton.addEventListener('click', () => {
 
 });
 
+
 removeButton.addEventListener('click', () => {
     removeElements(options);
 });
+
+
+/*Сортировка */
+
+
+sortButton.addEventListener('click', () => {
+    let arrTasks = [];
+    let inputs = document.querySelectorAll('.list_points');
+
+    for (let i = 0; i < inputs.length; i++) {
+        arrTasks.push(inputs[i].value);
+        if (!arrow.style.flexDirection || arrow.style.flexDirection === 'column') {
+            arrow.style.flexDirection = 'column-reverse';
+            triangle.style.transform = 'rotate(-180deg)';
+            triangle.style.marginBottom = '-2px';
+            triangle.style.marginLeft = '1px';
+
+        arrTasks.sort((a, b) => {
+            if (a < b) {
+                return -1;
+            }
+            if (a > b) {
+                return 1;
+            }
+            return 0;
+        });
+
+      for (let j = 0; j < arrTasks.length; j++) {
+        inputs[j].value = arrTasks[j];
+    }
+    
+} else {
+    arrow.style.flexDirection = 'column';
+    triangle.style.transform = 'none';
+    triangle.style.marginBottom = '0';
+    triangle.style.marginLeft = '0';
+    
+    arrTasks.sort((a, b) => {
+        if (a < b) {
+            return 1;
+        }
+        if (a > b) {
+            return -1;
+        }
+        return 0;
+    });
+    for (let j = 0; j < arrTasks.length; j++) {
+        inputs[j].value = arrTasks[j];
+    }
+}
+
+}
+});
+
+
+
+
+
+
+
+
+
+
 
