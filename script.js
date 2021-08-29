5e494035e34f0970f016ff9f7784157d7f5383eb
Dronrus86 <pop4666@yandex.ru>
2021-08-29 18:25:23 +0300

    add enter

--- script.js
+++ script.js
@@ -1,102 +1,111 @@
 
-const addButton = document.querySelector('.add');
-const todoList = document.querySelector('.to-do_list');
-const options = document.querySelector('.options');
-const removeButton = document.querySelector('.pushbutton_delete');
-const arrow = document.querySelector('.sort_arrow');
-const triangle = document.querySelector('.sorting_triangle');
-let sortButton = document.querySelector('.sorting');
-let form = document.querySelector('.form');
+const sortingButton = document.getElementById('sorting');
+//const add = document.getElementById('add');
+//const resetButton = document.getElementById('deleteTask');
+const form = document.getElementById('form');
+const plusButton = document.querySelector('.plus');
+const taskRow = document.getElementById('newTask');
+const taskListElement = document.querySelector('.task');
+const ulTaskListElement = document.querySelector('.task ul');
 
 
 
-
-function removeElements(rem) {
-    let optionElements = document.querySelectorAll('.options');
-    if (optionElements.length > 1) {
-        rem.remove();
-   }
-}
-
-
-
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-    let newElement = document.createElement('div');
-    newElement.innerHTML = options.innerHTML;
-    newElement.classList.add('options');
-    todoList.append(newElement);
-    let removeButton = newElement.querySelector('.pushbutton_delete');
-    removeButton.addEventListener('click', () => {
-        removeElements(newElement);
-    });
-
-       });
-
-removeButton.addEventListener('click', () => {
-    removeElements(options);
-});
+form.addEventListener('submit', clickAddButton);
+function clickAddButton(event) {
+    event.preventDefault();
+    if (taskRow.value.trim() === '' || taskRow.value === null) {
+        return false;
+    } 
+    const newLi = document.createElement('li');
+    const liSpan = document.createElement('span');
+    newLi.classList.add('liItem');
+    liSpan.classList.add('spanItem');
+    liSpan.innerText=taskRow.value;
+    ulTaskListElement.append(newLi);
+    newLi.append(liSpan);
+    newLi.draggable=true;
+    liSpan.contentEditable=true;
 
 
-/*Сортировка */
-
-
-sortButton.addEventListener('click', () => {
-    let arrTasks = [];
-    let inputs = document.querySelectorAll('.list_points');
-
-    for (let i = 0; i < inputs.length; i++) {
-        arrTasks.push(inputs[i].value);
-        if (!arrow.style.flexDirection || arrow.style.flexDirection === 'column') {
-            arrow.style.flexDirection = 'column-reverse';
-            triangle.style.transform = 'rotate(-180deg)';
-            triangle.style.marginBottom = '-2px';
-            triangle.style.marginLeft = '1px';
-
-        arrTasks.sort((a, b) => {
-            if (a < b) {
-                return -1;
-            }
-            if (a > b) {
-                return 1;
-            }
-            return 0;
-        });
-
-      for (let j = 0; j < arrTasks.length; j++) {
-        inputs[j].value = arrTasks[j];
-    }
+    const newButton = document.createElement('span')
+    newButton.innerHTML='&times;';
+    newButton.classList.add('liButton');
+    newLi.append(newButton);
     
-} else {
-    arrow.style.flexDirection = 'column';
-    triangle.style.transform = 'none';
-    triangle.style.marginBottom = '0';
-    triangle.style.marginLeft = '0';
-    
-    arrTasks.sort((a, b) => {
-        if (a < b) {
-            return 1;
+    taskRow.value='';
+    taskListElement.style.display='block';
+    liEditButton();
+    liDeleteButton(newButton);
+}
+
+
+function liEditButton() {
+    const textfields = document.querySelectorAll('.spanItem'); 
+    for(i=0; i<textfields.length; i++){
+    textfields[i].addEventListener('keypress', function(e) {
+        if (e.shiftKey || e.which === 13) {
+            e.preventDefault();
+          } else if (this.textContent.length >= 25){
+            e.preventDefault();
+            return false;
         }
-        if (a > b) {
-            return -1;
+    }, false);
+}}
+
+
+function liDeleteButton(button) {
+    button.addEventListener('click', (event) => {
+        const liElements = document.querySelectorAll('.liItem');
+        if (liElements.length === 1) {
+            button.parentElement.remove();
+            event.stopPropagation();
+            taskListElement.style.display='none';
+        } else {
+            button.parentElement.remove();
+            event.stopPropagation();
         }
-        return 0;
     });
-    for (let j = 0; j < arrTasks.length; j++) {
-        inputs[j].value = arrTasks[j];
+}
+
+// Сортировка
+
+let flag = 0;
+sortingButton.addEventListener('click', function () {
+if (flag === 0) {
+    liSortingButton(ulTaskListElement);
+    flag = 1;
+  } else {
+    liSortingButtonBack(ulTaskListElement); 
+    flag = 0;
+  }
+ })
+
+function liSortingButton() {
+    const liElements = document.querySelectorAll('.liItem');
+    if (liElements.length >= 2) {
+    Array.from(ulTaskListElement.getElementsByTagName('li'))
+      .sort((a, b) => a.textContent.localeCompare(b.textContent))
+      .forEach(li => ulTaskListElement.appendChild(li));
+      sortingButton.style.background="url('img/Group\ 90.svg') no-repeat";
+    sortingButton.onmouseover = () => {
+        sortingButton.style.background="url('img/Group\ 91.svg') no-repeat";
     }
+    sortingButton.onmouseout = () => {
+        sortingButton.style.background="url('img/Group\ 90.svg') no-repeat";
+    }}
 }
 
+  function liSortingButtonBack() {
+    const liElements = document.querySelectorAll('.liItem');
+    if (liElements.length >= 2) {
+    Array.from(ulTaskListElement.getElementsByTagName('li'))
+      .sort((a, b) => b.textContent.localeCompare(a.textContent))
+      .forEach(li => ulTaskListElement.appendChild(li));
+      sortingButton.style.background="url('img/Group\ 74.svg') no-repeat";
+       sortingButton.onmouseover = () => {
+        sortingButton.style.background="url('img/Group\ 73.svg') no-repeat";
+    }
+    sortingButton.onmouseout = () => {
+        sortingButton.style.background="url('img/Group\ 74.svg') no-repeat";
+    }}
 }
-});
-
-
-
-
-
-   
-
-
-
-
-
